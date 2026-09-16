91b3c735f40a863f43b50b89315af6d27ad96492
slaraibkamran <slaraibkamran.101@gmail.com>
2026-09-16 19:36:20 +0700

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,5 +1,26 @@
 public class Demo {
 public static void main (String[] args){
-Student currentStudent = new Student();
-currentStudent = null;
-currentStudent.printSummary();}}
+Student s1 = new Student();
+s1.id = 28;
+s1.name = "Laraib";
+s1.completedCredits = 4;
+s1.printSummary();
+
+Student s2 = new Student();
+s2.id = 29;
+s2.name = "Batool";
+s2.completedCredits = 3;
+s2.printSummary();
+s2.addCredits(2);
+
+Student a = new Student();
+a.name = "Ali";
+Student b = a;
+b.name = "Sara";
+Student c = new Student();
+c.name = a.name;
+a.printSummary();
+b.printSummary();
+c.printSummary();
+
+}}
--- Student.java
+++ Student.java
@@ -7,4 +7,7 @@ public void printSummary(){
 System.out.println("ID: " + id);
 System.out.println("Name: " + name);
 System.out.println("Completed Credits: " + completedCredits);}
+
+public void addCredits(int amount){
+System.out.print("Added Credits: " + amount);}
 }
